d66e0eba13a3dc7ad6f043010dcf315248976e2e
ruan65 <ruan65@gmail.com>
2023-12-15 17:24:08 +0400

    draw cards

--- lib/components/card.dart
+++ lib/components/card.dart
@@ -1,6 +1,10 @@
+import 'dart:math';
+import 'dart:ui';
+
 import 'package:flame/components.dart';
 import 'package:syzygy/models/rank.dart';
 import 'package:syzygy/models/suit.dart';
+import 'package:syzygy/utils.dart';
 import 'package:syzygy/utils/sizes.dart';
 
 class Card extends PositionComponent {
@@ -19,6 +23,107 @@ class Card extends PositionComponent {
   bool get isFaceDown => !_faceUp;
 
   void flip() => _faceUp = !_faceUp;
+
+  static final Paint backBackgroundPaint = Paint()
+    ..color = const Color(0xff380c02);
+  static final Paint backBorderPaint1 = Paint()
+    ..color = const Color(0xffdbaf58)
+    ..style = PaintingStyle.stroke
+    ..strokeWidth = 10;
+  static final Paint backBorderPaint2 = Paint()
+    ..color = const Color(0x5CEF971B)
+    ..style = PaintingStyle.stroke
+    ..strokeWidth = 35;
+  static final RRect cardRRect = RRect.fromRectAndRadius(
+    cardSize.toRect(),
+    const Radius.circular(cardRadius),
+  );
+
+  static final RRect backRRectInner = cardRRect.deflate(40);
+  static final Sprite flameSprite = klondikeSprite(1367, 6, 357, 501);
+
+  static final Paint frontBackgroundPaint = Paint()
+    ..color = const Color(0xff000000);
+  static final Paint redBorderPaint = Paint()
+    ..color = const Color(0xffece8a3)
+    ..style = PaintingStyle.stroke
+    ..strokeWidth = 10;
+  static final Paint blackBorderPaint = Paint()
+    ..color = const Color(0xff7ab2e8)
+    ..style = PaintingStyle.stroke
+    ..strokeWidth = 10;
+
+  static final Sprite redJack = klondikeSprite(81, 565, 562, 488);
+  static final Sprite redQueen = klondikeSprite(717, 541, 486, 515);
+  static final Sprite redKing = klondikeSprite(1305, 532, 407, 549);
+
+  static final blueFilter = Paint()
+    ..colorFilter = const ColorFilter.mode(
+      Color(0x880d8bff),
+      BlendMode.srcATop,
+    );
+  static final Sprite blackJack = klondikeSprite(81, 565, 562, 488)
+    ..paint = blueFilter;
+  static final Sprite blackQueen = klondikeSprite(717, 541, 486, 515)
+    ..paint = blueFilter;
+  static final Sprite blackKing = klondikeSprite(1305, 532, 407, 549)
+    ..paint = blueFilter;
+
+  @override
+  void render(Canvas canvas) {
+    if (_faceUp) {
+      _renderFront(canvas);
+    } else {
+      _renderBack(canvas);
+    }
+  }
+
+  void _renderFront(Canvas canvas) {
+    canvas.drawRRect(cardRRect, frontBackgroundPaint);
+    canvas.drawRRect(
+      cardRRect,
+      suit.isRed ? redBorderPaint : blackBorderPaint,
+    );
+
+    final rankSprite = suit.isBlack ? rank.blackSprite : rank.redSprite;
+    final suitSprite = suit.sprite;
+    _drawSprite(canvas, rankSprite, 0.1, 0.08);
+    _drawSprite(canvas, rankSprite, 0.1, 0.08, rotate: true);
+    _drawSprite(canvas, suitSprite, 0.1, 0.18, scale: 0.5);
+    _drawSprite(canvas, suitSprite, 0.1, 0.18, scale: 0.5, rotate: true);
+  }
+
+  void _renderBack(Canvas canvas) {
+    canvas.drawRRect(cardRRect, backBackgroundPaint);
+    canvas.drawRRect(cardRRect, backBorderPaint1);
+    canvas.drawRRect(backRRectInner, backBorderPaint2);
+    flameSprite.render(canvas, position: size / 2, anchor: Anchor.center);
+  }
+
+  void _drawSprite(
+      Canvas canvas,
+      Sprite sprite,
+      double relativeX,
+      double relativeY, {
+        double scale = 1,
+        bool rotate = false,
+      }) {
+    if (rotate) {
+      canvas.save();
+      canvas.translate(size.x / 2, size.y / 2);
+      canvas.rotate(pi);
+      canvas.translate(-size.x / 2, -size.y / 2);
+    }
+    sprite.render(
+      canvas,
+      position: Vector2(relativeX * size.x, relativeY * size.y),
+      anchor: Anchor.center,
+      size: sprite.srcSize.scaled(scale),
+    );
+    if (rotate) {
+      canvas.restore();
+    }
+  }
 
   @override
   String toString() => rank.label + suit.label; // e.g. "Q♠" or "10♦"
